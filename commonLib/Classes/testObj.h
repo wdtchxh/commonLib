76fb8d65ba7dfea7312e284aacae4f5d88b076fd
Yang Redlei <yanghonglei@emoney.cn>
2016-10-26 04:06:49 -0500

    test

---
+++ commonLib/Classes/testObj.h
@@ -0,0 +1,14 @@
+//
+//  testObj.h
+//  Pods
+//
+//  Created by yanghonglei on 2016/10/26.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testObj : NSObject
+
+-(void)testFun;
+@end
---
+++ commonLib/Classes/testObj.m
@@ -0,0 +1,16 @@
+//
+//  testObj.m
+//  Pods
+//
+//  Created by yanghonglei on 2016/10/26.
+//
+//
+
+#import "testObj.h"
+
+@implementation testObj
+
+-(void)testFun{
+    NSLog(@"test branch work");
+}
+@end
